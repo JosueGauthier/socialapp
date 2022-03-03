df836418c825a849387b13b259ff1e378c2c317f
Josue Gauthier <josue.gauthier@gmail.com>
2022-03-03 18:14:19 +0100

    install server on localhost OK

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,23 @@
-console.log("This thing is working");
+//console.log("This thing is working V");
+
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.set("port",port);
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req,res) =>{
+
+    res.send({
+        data: "the social rebuild API"
+    });
+
+});
+
+app.listen(app.get("port"), () => {
+    console.log(`🚀 is rocking over http://localhost:${app.get("port")}`);
+
+})
