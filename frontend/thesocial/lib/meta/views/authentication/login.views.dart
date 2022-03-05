656a127f0c18c57b3f0c704bba6ad7862e145925
Josue Gauthier <josue.gauthier@gmail.com>
2022-03-05 10:55:25 +0100

    add initialmain.dart

---
+++ frontend/thesocial/lib/meta/views/authentication/login.views.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final emailcontroller = TextEditingController();
+    final passwordcontroller = TextEditingController();
+    return Scaffold(
+        body: Container(
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextField(
+            controller: emailcontroller,
+            decoration: InputDecoration(hintText: "Enter email"),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: passwordcontroller,
+            decoration: InputDecoration(hintText: "Enter password"),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const MaterialButton(
+            child: Text("login"),
+            onPressed: null,
+            color: Colors.red,
+          ),
+        ],
+      )),
+    ));
+  }
+}
