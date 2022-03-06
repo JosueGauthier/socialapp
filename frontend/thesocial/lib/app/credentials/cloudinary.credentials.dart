581f404327a0ca4a6964abe8451ba7b33abaf781
Josue Gauthier <josue.gauthier@gmail.com>
2022-03-06 11:14:04 +0100

    add gitignore file

--- frontend/thesocial/lib/app/credentials/cloudinary.credentials.dart
+++ frontend/thesocial/lib/app/credentials/cloudinary.credentials.dart
@@ -1,11 +1,6 @@
 class CloudinaryCredentials {
-
-  static const APIKEY = "TO ENTER";
-  static const APISecret = "TO ENTER";
-  static const APIEnv = "TO ENTER";
-  static const APIName ="TO ENTER";
-
-
-
-
+  static const APIKEY = "";
+  static const APISecret = "";
+  static const APIEnv = "";
+  static const APIName = "";
 }
