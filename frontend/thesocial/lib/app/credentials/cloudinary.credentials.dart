296f72081b206bbb8a42cf69643ddaf625c68a1c
Josue Gauthier <josue.gauthier@gmail.com>
2022-03-06 11:30:36 +0100

    df

--- frontend/thesocial/lib/app/credentials/cloudinary.credentials.dart
+++ frontend/thesocial/lib/app/credentials/cloudinary.credentials.dart
@@ -1,6 +1,7 @@
 class CloudinaryCredentials {
-  static const APIKEY = "";
-  static const APISecret = "";
-  static const APIEnv = "   ";
-  static const APIName = "";
+  static const APIKEY = "917935464313987";
+  static const APISecret = "bmizwx7JaQFFFKke6QxijnxeLvI";
+  static const APIEnv =
+      "CLOUDINARY_URL=cloudinary://917935464313987:bmizwx7JaQFFFKke6QxijnxeLvI@dxsy9rszs";
+  static const APIName = "dxsy9rszs";
 }
